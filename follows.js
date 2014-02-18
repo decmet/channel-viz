b0b559c45b48365edc1f1670f6dedc8fde3f4263
decmet <dominic@decmetrics.co.uk>
2014-02-18 16:43:08 +0000

    Added default Keys

--- follows.js
+++ follows.js
@@ -12,12 +12,12 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+	var defaultKey		= 'tKs1Tk5UseXzOntqiytPUROVsdPWuYYYTe2ANwuHR69rrckn', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [166956417], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'Dom Logo', // Replaces Xively logo in the header
+		dataDuration	= '1', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+		dataInterval	= 60, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0000FF', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
